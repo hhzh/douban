84421bb135ccbe3e154a87b7573203f50465a2c5
hhzh <1138253815@qq.com>
2017-11-09 19:34:10 +0800

    add

---
+++ douban/spiders/movie.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class MovieSpider(scrapy.Spider):
+    name = "movie"
+    allowed_domains = ["www.movie.douban.com"]
+    start_urls = ['http://www.movie.douban.com/']
+
+    def parse(self, response):
+        
